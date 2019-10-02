e9c0e3619d379a17dc9d9e8627ba1d80610b4aaa
k-nicha <45272352+k-nicha@users.noreply.github.com>
2019-10-02 21:47:17 +0200

    add lifecycle methods

--- src/Wrapper.js
+++ src/Wrapper.js
@@ -1,18 +1,45 @@
 import React from 'react'
 import User from './User'
-import data from './data.js'
+import Error from './Error'
+import axios from 'axios'
 
-const Wrapper = (props) => {
-    let users = []
-    users = data.map(element => {
-        return <User key={element.id} name={element.name} email={element.email} /> 
-    })
-    return (
-        <React.Fragment>
-            {props.children}
-            {users}
-        </React.Fragment>
-    )
+class Wrapper extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            data: null
+        }
+    }
+
+    componentDidMount () {
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then((response) => {
+                const users = response.data.map((element) => {
+                    return <User
+                        key={element.id}
+                        name={element.name}
+                        email={element.email}
+                    />
+                })
+                this.setState({ data: users })
+            })
+            .catch((error) => {
+                this.setState({ data: <Error /> })
+            })
+    }
+    
+    render () {
+        return (
+            <React.Fragment>
+                {this.props.children}
+                {
+                    !this.state.data
+                    ? <h6> LOADING... </h6>
+                    : this.state.data
+                }
+            </React.Fragment>
+        )
+    }
 }
 
 export default Wrapper
